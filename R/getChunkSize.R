43bca577bfe77d69d93d9324aa9e92d7fe93100b
Beatrice Marti <martibeatrice@gmail.com>
2021-06-28 20:29:58 +0200

    implement getChunkSize for monthly recording time steps

--- R/getChunkSize.R
+++ R/getChunkSize.R
@@ -8,6 +8,7 @@
 #' @param end_date Lubridate datetime of the end of the simulation
 #' @param recordingTimeStep The simulations recording time step (in seconds) in character or as numeric.
 #' @return A numeric of the number of time steps, including one header line, to be read.
+#' @note Currently only sub-daily, daily and monthly time steps are implemented.
 #' @examples
 #' start_date <- lubridate::as_datetime("01.20.2021 00:00:00",
 #'                                      format = "%m.%d.%Y %H:%M:%S")
@@ -17,6 +18,18 @@
 #' chunk_size <- getChunkSize(start_date, end_date, recordingTimeStep)
 #' @export
 getChunkSize <- function(start_date, end_date, recordingTimeStep) {
-  chunk_size <- lubridate::interval(start_date, end_date) /
-    lubridate::seconds(1) / as.numeric(recordingTimeStep) + 2
+
+  if (recordingTimeStep <= 86400) {
+    # Sub-daily to daily time steps
+    chunk_size <- lubridate::interval(start_date, end_date) /
+      lubridate::seconds(1) / as.numeric(recordingTimeStep) + 2
+  } else if (recordingTimeStep == 2628000 | recordingTimeStep == 2592000) {
+    # Montly time steps (365*24*60*60/12 | 60*60*24*30)
+    chunk_size <- base::length(base::seq(start_date, end_date, "month")) + 2
+  } else {
+    cat("recordingTimeStep =", recordingTimeStep, "is currently not implemented.")
+    return(NULL)
+  }
+  return(chunk_size)
+
 }
--- tests/testthat/test_getChunkSize.R
+++ tests/testthat/test_getChunkSize.R
@@ -6,3 +6,13 @@ test_that("getChunkSize returns as expected", {
   recordingTimeStep <- 3600
   expect_equal(122, getChunkSize(start_date, end_date, recordingTimeStep))
 })
+
+
+test_that("getChunkSize returns as expected for Sokh parameters", {
+  start_date <- lubridate::as_datetime("01.01.1991 01:00:00",
+                                       format = "%m.%d.%Y %H:%M:%S")
+  end_date <- lubridate::as_datetime("12.31.2013 23:00:00",
+                                     format = "%m.%d.%Y %H:%M:%S")
+  recordingTimeStep <- 2592000  # Monthly
+  expect_equal(278, getChunkSize(start_date, end_date, recordingTimeStep))
+})
