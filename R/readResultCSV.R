67c13d3ba406604a5cb3ce59e92afa3cf264d2ad
Bea Marti <mabesa@users.noreply.github.com>
2021-07-08 22:54:11 +0200

    Work on portability of readResultCSV.R
    now can also read ;-separated files, not only ,-separated files (system locale dependent...)

--- R/readResultCSV.R
+++ R/readResultCSV.R
@@ -1,12 +1,30 @@
 #' Reads RS Minerve result file and returns data.
 #'
-#' @param filename Path to file to be read. It is any csv file that is written by saving data from the Selection and plots tab in RSMinerve to a csv file.
-#' @return data Tibble with data from file
+#' @param filename Path to file to be read. It is any csv file that is written by saving data from the Selection and plots tab in RSMinerve to a csv file. Depending on the systems locale, the csv may use comas or semi-colons as separators. The decimal separator must be a point, commas are not recognized.
+#' @return data Tibble with data from file. Null if file not found or cant be read.
 #' @note An example RSMinerve results csv file is available under \url{http://raw.githubusercontent.com/hydrosolutions/RSMinerveR/main/tests/testthat/test_translateCSVtoDST.csv}. If the function generates funny numbers, please check the csv file. The decimal separators must be \code{.} and not \code{,}. If you find commas please change the settings of your PC (Control Panel, Clock and Region, Region, Additional settings, select \code{.} for the Decimal symbol, press "Apply" and close the Control Panel windows. and RSMinerve will write \code{,}.
 #' @export
 #'
 readResultCSV <- function(filename) {
-  data <- readr::read_csv(filename, skip = 1, col_names = T)
+
+  if (!file_test("-f", filename)) {
+    cat("ERROR: File", filename, "not found.\n")
+    return(NULL)
+  }
+
+  # Test delimiter of csv file
+  test <- readr::read_csv(filename, skip = 2, col_names = F)
+  if (dim(test)[2] == 1) {
+
+    # Not tab delimited, try semi-colon
+    data <- readr::read_csv2(filename, skip = 1, col_names = T)
+
+  } else {
+
+    data <- readr::read_csv(filename, skip = 1, col_names = T)
+
+  }
+
   colnames(data)[1] <- "date"
   data$date <- as.POSIXct(data$date, format = "%d.%m.%Y %H:%M:%S")
   data <- data |>
--- tests/testthat/test_readResultCSV.R
+++ tests/testthat/test_readResultCSV.R
@@ -1,16 +1,39 @@
 test_that("readResultsCSV returns expected", {
+
   filepath <- normalizePath(file.path("test_readResultCSV.csv"))
   result <- readResultCSV(filepath)
 
-  # expect_false(is.null(result))
-  #
-  # testvalue <- result %>%
-  #   dplyr::filter(Variable == "Station:ETP",
-  #                 Object == "Estacion SOCONT 3",
-  #                 Datetime == lubridate::as_datetime("2013-09-09 00:00:00"))
-  #
-  # expect_equal(12168, dim(result)[1])
-  # expect_equal(4, dim(result)[2])
-  # expect_equal(0, testvalue$Value)
+  expect_false(is.null(result))
+
+  testvalue <- result |>
+    dplyr::filter(model == "GSM 1",
+                  variable == "Qglacier",
+                  date == lubridate::as_datetime("2013-09-09 00:00:00",
+                                                 tz = "CEST"))
+
+  expect_equal(169, dim(result)[1])
+  expect_equal(5, dim(result)[2])
+  expect_equal(0.02953923, testvalue$value)
+
+})
+
+
+test_that("readResultsCSV also reads ;-separated files", {
+
+  filepath <- normalizePath(file.path("test_readResultCSV_semicolon.csv"))
+  result <- readResultCSV(filepath)
+
+  expect_false(is.null(result))
+
+  testvalue <- result |>
+    dplyr::filter(model == "So_Sokh_eb20",
+                  variable == "Qglacier",
+                  date == lubridate::as_datetime("1981-02-01 01:00:00",
+                                                 tz = "CET"))
+
+  expect_equal(1634, dim(result)[1])
+  expect_equal(5, dim(result)[2])
+  expect_equal(0.00118812, testvalue$value)
+
 
 })
