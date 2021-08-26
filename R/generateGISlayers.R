827c3d2bf03ec306db40f900e2103d4b06ae4d44
Bea Marti <mabesa@users.noreply.github.com>
2021-08-26 15:48:04 +0200

    temporary work

---
+++ R/generateGISlayers.R
@@ -0,0 +1,14 @@
+#' Generates GIS layers for RS Minerve
+#'
+#' This function calls the QGIS processor available on QGIS 3.16 or higher where
+#' it can execute native QGIS functions, as well as SAGA and grass7 algorithms.
+#'
+#' @details This function requires setup of coupling of the QGIS processor to
+#' R. Please refer to Vignette
+#'
+#' @param
+#' @return 0 for success, 1 for failure.
+#' @export
+generateGISlayers <- function() {
+
+}
